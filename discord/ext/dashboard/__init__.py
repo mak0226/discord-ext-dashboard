6ea1ce4633fe2dac1c4928331d138fdd6544b083
PenguinMaster0226 <71048181+PenguinMaster0226@users.noreply.github.com>
2021-02-09 22:41:10 -0500

    Update __init__.py

--- discord/ext/dashboard/__init__.py
+++ discord/ext/dashboard/__init__.py
@@ -1,4 +1,4 @@
-from .dashboard import Dashboard
-from .server import Server
+from .dashboard import QuartDashboard, WebhookDashboard
+from .server import QuartServer, WebhookServer
 
 __version__ = "v0.5.0"
